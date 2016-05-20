28469f8b90a261cb18967b7bfdfc421fd88f76d8
lukaswhite <hello@lukaswhite.com>
2016-05-20 13:40:40 +0100

    Initial commit

--- public/assets/scripts.js
+++ public/assets/scripts.js
@@ -1,6 +1,5 @@
 Vue.http.options.root = '/api';
 
-
 Vue.filter('time', function ( hour ) {
   if ( hour < 10 ) {
   	return '0' + hour + ':00';	
@@ -38,103 +37,15 @@ var widget = new Vue({
 	},
 
 	ready: function() {        
-    var m = moment(this.month);
-    m.date(1);
-    console.log(m.weekday());
 
     this.getAvailabilityForMonth();
 
   },
 
-	computed : 	{
-		selectedMonth : {
-			cache: false,
-			get : function() {
-				return this.month.format( 'MMM YYYY' );
-			}
-		},
-		MMdays : function(){
-			
-			var temp = moment( this.month ).date( 1 );
-			var days = [];
-			var m = temp.month();
-			var now = moment();
-
-			do {					
-				days.push({
-					d : temp.date(),
-					past : temp.isBefore( now, 'day' ),
-					today : temp.isSame( now, 'day' )
-				});
-				temp.add(1, 'day');
-			} while ( temp.month() == m );
-
-			return days;
-
-		},
-		_calendar : {
-			cache : false,
-			get : function() {
-				var rows = [];				
-				var temp = moment( this.month ).date( 1 );
-
-				var items = [];
-
-				// Some padding at the beginning
-				for ( var p = 0; p < temp.weekday(); p++ )
-				{					
-					items.push({
-						d : null
-					});
-				}				
-				
-				// Merge in the array of days
-				items.push.apply( items, this.days );
-
-				// Some padding at the end
-				if ( items.length % 7 ) {
-					for ( var p = ( 7 - ( items.length % 7 ) ); p > 0; p-- )
-					{					
-						items.push({
-							d : null
-						});
-					}
-				}
-
-				// Split the array int chunkc of seven
-				var rows = items.map( function(e,i){ 
-    			return i%7===0 ? items.slice(i,i+7) : null; 
-				})
-				.filter(function(e){ return e; });
-
-				return rows;
-			}
-		}
-		/**
-		hours : function() {
-			var hours = [];
-
-			var startAt = 9;
-			var endAt = 17;
-
-			for ( var h = 7; h <= 19; h++ ) {
-				hours.push({
-					h : h,					
-					working : ( h >= startAt && h <= endAt ),
-					available : !!Math.floor(Math.random() * 2)
-				});
-			}
-			console.log( hours );
-			return hours;
-		}
-		**/
-	},
-
 	methods : {
 
 		/**
 		 * Fetch the availability data for a given month
-		 * @return {[type]} [description]
 		 */
 		getAvailabilityForMonth : function( ) {
 			
@@ -166,6 +77,11 @@ var widget = new Vue({
 
 			});
 		},
+
+		/**
+		 * Build the calendar; takes an array of days and "pads" before and after, splitting those cells into rows
+		 * of seven.
+		 */
 		buildCalendar : function() 
 		{			
 			var rows = [];							
@@ -192,7 +108,7 @@ var widget = new Vue({
 				}
 			}
 
-			// Split the array int chunkc of seven
+			// Split the array into "chunks" of seven
 			var rows = items.map( function( e, i ) { 
   			return i % 7 === 0 ? items.slice( i, i + 7 ) : null; 
 			})
@@ -200,18 +116,30 @@ var widget = new Vue({
 
 			this.$set( 'calendar', rows );
 		},		
+
+		/**
+		 * Navigate to the previous month
+		 */
 		previousMonth : function() {			
 			var current = this.month;
 			this.$set( 'month', null )
 			this.$set( 'month', current.subtract( 1, 'months' ) );		
 			this.getAvailabilityForMonth();	
 		},
+
+		/**
+		 * Navigate to the next month
+		 */
 		nextMonth : function() {			
 			var current = this.month;
 			this.$set( 'month', null )
 			this.$set( 'month', current.add( 1, 'months' ) );			
 			this.getAvailabilityForMonth();
 		},
+
+		/**
+		 * Called when the user selects a day; this will go grab a list of available times.
+		 */
 		selectDay : function( d ) {
 			
 			if ( d ) {
@@ -219,7 +147,7 @@ var widget = new Vue({
 				var day = moment( this.month ).date ( d );
 
 				// If it's in the past, exit
-				if ( day.isBefore( moment(), 'day ') ) {
+				if ( day.isBefore( moment(), 'day' ) ) {
 					return;
 				}
 
@@ -240,13 +168,15 @@ var widget = new Vue({
 					}
 
 				});
-
-				//this.$recompute('hours')
+				
 				this.time = null;
 			}
 		},
 		
-		selectHour : function( hour ) {		
+		/**
+		 * Called when the user selects an available time.
+		 */
+		selectTime : function( hour ) {		
 
 			if ( hour.available ) {
 			
@@ -256,6 +186,9 @@ var widget = new Vue({
 
 		},
 		
+		/**
+		 * Call back for the booking form submission - make the booking.
+		 */
 		confirm : function( )
 		{
 			if ( this.bookingForm.$valid ) {
@@ -272,12 +205,13 @@ var widget = new Vue({
 					
 				});
 
-			} else {
-				console.log( 'form is invalid' );
 			}
 
 		},
 
+		/**
+		 * Reset the process and start over.
+		 */
 		reset : function() 
 		{
 
